8c7071370eab570b7fb369a82169bc9add177c3c
ignored-comment <info.konham@gmail.com>
2021-04-12 13:47:22 -0400

    Create test assertions for Search method on Actor model

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -6,6 +6,22 @@ RSpec.describe Actor do
     it {should have_many(:movies).through(:castings)}
   end
 
+  describe '#search' do
+    it 'returns partial matches' do
+      mgm = Studio.create!(name: "MGM", location: "Hollywood")
+      die_another_day = mgm.movies.create!(title: "Die Another Day", creation_year: 2002, genre: "Action Thriller")
+      pierce_brosnan = die_another_day.actors.create!(name: "Pierce Brosnan", age: 67, currently_working: true)
+      halle_berry = die_another_day.actors.create!(name: "Halle Berry", age: 54, currently_working: true)
+      never_was_in_this_movie = die_another_day.actors.create!(name: "Kon Ham", age: 31, currently_working: false)
+
+      golden_eye = mgm.movies.create!(title: "Goldeneye", creation_year: 1995, genre: "Action Thriller")
+      pierce_brosnan_again = golden_eye.actors.create!(name: "Pierce Brosnan", age: 67, currently_working: true)
+      famke_janssen = golden_eye.actors.create!(name: "Famke Janssen", age: 56, currently_working: true)
+
+      expect(Actor.search("Famke")).to eq([famke_janssen])
+    end
+  end
+
   it 'can generate list of actors that are unique, ordered oldest to youngest, and currently working is true' do
     mgm = Studio.create!(name: "MGM", location: "Hollywood")
     die_another_day = mgm.movies.create!(title: "Die Another Day", creation_year: 2002, genre: "Action Thriller")
